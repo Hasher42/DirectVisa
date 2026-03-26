a1094ca63ab0b7bd0eddf035fc67cf20e563b49b
Hasher Hanif <mv5hasher@gmail.com>
2026-03-26 16:58:41 +0500

    first commit

--- DirectVisa/obj/Debug/net8.0/DirectVisa.AssemblyInfo.cs
+++ DirectVisa/obj/Debug/net8.0/DirectVisa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DirectVisa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df39e22a4a596b8c8c2e3c83ca854457f85aa7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DirectVisa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DirectVisa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
